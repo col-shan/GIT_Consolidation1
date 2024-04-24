a6a374faa28f791fd3a61be0901f53840039ec79
Colin Shanahan <colshan@umd.edu>
2024-04-23 23:00:01 -0400

    reworked existing code when dealing cards so that each card is shown separately from the rest; added randomized choice from dealer on Suits or Ranks choice

--- Card_Memory_Game.py
+++ Card_Memory_Game.py
@@ -1,43 +1,52 @@
 #creating a card game based on memorization
-#there will be 5 rounds (best of 5)
+#each card in my deck will contain the typical suits and ranks values
+#the dealer will flip 4 cards at a time, with each one displayed for only 2 seconds
+#there will be 5 rounds
 #the player will score points by remembering specific aspects of cards that they were shown
-#the cards will only show for 2 seconds at a time
 
 #import file paths
 import os
-#import so I can randomize the cards
+#import so I can randomize the cards and suit or ranks the player needs to guess
 import random
+#import so I can set time limit on how long each card is shown for
+import time
+#import the deck of cards I created
+import deck
 
-#must create function with 52 values for my deck
-#will use tuples, with cards as ints and suits as strings
-def create_deck():
-    #create my 2 lists consisting of suits and ranks
-    suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-    #range must add 1, so because i want 2 through 10 i must do 2-11 as 11 isn't included! that took a while to figure out lol
-    ranks = list(range(2, 11)) + ['Jack', 'Queen', 'King', 'Ace']
-    #now loop my lists together in every rank and suit combination to my tuples of every possible card combination
-    deck = [(rank, suit) for suit in suits for rank in ranks]
-    return deck
-
-#have now created a list of tuples to create my deck! time to print it
-deck = create_deck()
-print(deck)
-
-random.shuffle(deck)
+my_deck = deck.create_deck()
+#use inbuilt shuffle module in random to shuffle my deck (randomize card dealt)
+random.shuffle(my_deck)
 
 #must now simulate what a dealer does by moving one card from my deck to a players hand
 #create my arguments for both my deck of cards, and the hand of the player
-def deal_card(deck, hand):
+def deal_card(my_deck, hand):
     #used pop to permanently remove card from my deck (no repeating cards)
-    card = deck.pop()
+    card = my_deck.pop()
     #use append to assign card to the players hand
     hand.append(card)
 
 #create my player's empty hand
 player_hand = []
 
-#deal a card to the player's hand
-deal_card(deck, player_hand)
+#deal 4 cards to the player's hand
+for _ in range(4):
+    deal_card(my_deck, player_hand)
+    #print out the 4 cards indivually instead of all grouped together
+    for card in player_hand:
+        rank, suit = card
+    #print out what card the player got
+    print("You drew:", rank, "of", suit)
+    #show cards for 2 seconds (looked up how to do this and screen clear via StackOverflow)
+    time.sleep(2)
+    #dealer must take back cards after they are dealt (memorization aspect, cards must disappear(clears the terminal screen))
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-#print out what card the player got
-print("Player's Hand:", player_hand)
+#dealer randomly selects suits or ranks for player to guess
+suits_or_ranks = random.choice(["Suits", "Ranks"])
+print("You need to guess the ", suits_or_ranks)
+
+#player must list out suits or ranks depending on the dealer's random choice
+if suits_or_ranks == "Suits":
+    player_guess = input("List the suits (Hearts, Diamonds, Clubs, Ace) of the cards that were dealt: ")
+else:
+    player_guess = input("List the ranks (2 through 10 or Jack/Queen/King/Ace) of the cards that were dealt: ")
---
+++ deck.py
@@ -0,0 +1,14 @@
+#Deck of Cards
+
+#must create function with 52 values for my deck
+#will use tuples, with cards as ints and suits as strings
+def create_deck():
+    #create my 2 lists consisting of suits and ranks
+    suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+    #range must add 1, so because i want 2 through 10 i must do 2-11 as 11 isn't included! that took a while to figure out lol
+    ranks = list(range(2, 11)) + ['Jack', 'Queen', 'King', 'Ace']
+    #now loop my lists together in every rank and suit combination to my tuples of every possible card combination
+    deck = [(rank, suit) for suit in suits for rank in ranks]
+    return deck
+
+#will now impport this module containing my deck of cards into my main code
